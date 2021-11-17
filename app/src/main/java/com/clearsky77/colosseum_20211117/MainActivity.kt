da818d874a40cefb8345d5a3c6e269add672649d
clearsky77 <clearsky3264@gmail.com>
2021-11-18 08:39:03 +0900

    서버가 주는 code Int값 추출   - 로그인 성공/실패 판단 근거로 사용   - 토스트 (UI 조작) 하려면 앱 중단됨 -> runOnUiThread { } 안에서 실행

--- app/src/main/java/com/clearsky77/colosseum_20211117/MainActivity.kt
+++ app/src/main/java/com/clearsky77/colosseum_20211117/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.clearsky77.colosseum_20211117
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.clearsky77.colosseum_20211117.databinding.ActivityMainBinding
 import com.clearsky77.colosseum_20211117.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -25,6 +28,26 @@ class MainActivity : BaseActivity() {
 
             // 로그인 요청. 이메일과 비번이 맞는가?
             ServerUtil.postRequestLogIn(inputEmail, inputPw, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+//                    로그인 API를 호출하고 돌아온 상황
+//                    결과로 jsonObj 하나를 받아서 돌아온 상황
+
+                    val code =  jsonObj.getInt("code")
+
+//                    code : 200 -> 로그인 성공 토스트
+//                    그 외 -> 로그인 실패 토스트
+                    runOnUiThread {
+                    // 백그라운드에서 UI를 쓰려고 하면 오류라 감지하고 앱이 중단된다. 그 해결방안으로 runOnUiThread를 사용한다.
+                        if (code == 200) {
+                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        }
+                        else {
+                            Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+                }
 
             })
         }
