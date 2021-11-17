e3ed65b613f8539c2912c1af567ccb86dcf61596
clearsky77 <clearsky3264@gmail.com>
2021-11-18 08:52:02 +0900

    로그인할 때 서버가 주는 message를 UI에 띄우기

--- app/src/main/java/com/clearsky77/colosseum_20211117/MainActivity.kt
+++ app/src/main/java/com/clearsky77/colosseum_20211117/MainActivity.kt
@@ -33,17 +33,20 @@ class MainActivity : BaseActivity() {
 //                    결과로 jsonObj 하나를 받아서 돌아온 상황
 
                     val code =  jsonObj.getInt("code")
+                    val message =  jsonObj.getString("message")
+//                    val code =  jsonObj.getJSONObject("data").getJSONObject("user").getString("nick_name")
 
 //                    code : 200 -> 로그인 성공 토스트
 //                    그 외 -> 로그인 실패 토스트
                     runOnUiThread {
                     // 백그라운드에서 UI를 쓰려고 하면 오류라 감지하고 앱이 중단된다. 그 해결방안으로 runOnUiThread를 사용한다.
                         if (code == 200) {
-                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
                         }
                         else {
-                            Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
                         }
+//                            Toast.makeText(mContext, code, Toast.LENGTH_SHORT).show()
 
                     }
 
