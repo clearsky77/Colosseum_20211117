61f4a149c5fbd9cbcdde6a5fb4deb5fc33e333ed
clearsky77 <clearsky3264@gmail.com>
2021-11-18 11:31:42 +0900

    GET API 활용 방법 체험 마무리  - 응답 분석 (code 값 추출) -> 텍스트뷰의 문구 반영에 활용

--- app/src/main/java/com/clearsky77/colosseum_20211117/SignUpActivity.kt
+++ app/src/main/java/com/clearsky77/colosseum_20211117/SignUpActivity.kt
@@ -24,7 +24,25 @@ class SignUpActivity : BaseActivity() {
 //        이메일 중복인지 확인
         binding.btnEmailCheck.setOnClickListener {
             val inputEmail = binding.edtEmail.text.toString()
-            ServerUtil.getRequestDuplCheck("EMAIL",inputEmail,null)
+            ServerUtil.getRequestDuplCheck("EMAIL",inputEmail,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    val code = jsonObj.getInt("code")
+
+                    runOnUiThread {
+
+                        if (code == 200) {
+//                        사용해도 좋다
+                            binding.txtEmailCheckResult.text = "사용해도 좋습니다."
+                        }
+                        else {
+//                        사용하면 안됨
+                            binding.txtEmailCheckResult.text = "중복된 이메일입니다. 다른 이메일로 검사해주세요."
+                        }
+
+                    }
+                }
+
+            })
         }
 
 
