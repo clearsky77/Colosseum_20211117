8fa7f0862a5005cedcbdec6d18cf751675527538
clearsky77 <clearsky3264@gmail.com>
2021-11-18 10:51:36 +0900

    연습문제. 로그인 성공시 -> 닉네임 추출. 토스트.

--- app/src/main/java/com/clearsky77/colosseum_20211117/MainActivity.kt
+++ app/src/main/java/com/clearsky77/colosseum_20211117/MainActivity.kt
@@ -29,32 +29,37 @@ class MainActivity : BaseActivity() {
             val inputPw = binding.edtPassword.text.toString()
 
             // 로그인 요청. 이메일과 비번이 맞는가?
-            ServerUtil.postRequestLogIn(inputEmail, inputPw, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(jsonObj: JSONObject) {
+            ServerUtil.postRequestLogIn(
+                inputEmail,
+                inputPw,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObj: JSONObject) {
 //                    로그인 API를 호출하고 돌아온 상황
 //                    결과로 jsonObj 하나를 받아서 돌아온 상황
 
-                    val code =  jsonObj.getInt("code")
-                    val message =  jsonObj.getString("message")
+                        val code = jsonObj.getInt("code")
+                        val message = jsonObj.getString("message")
 //                    val code =  jsonObj.getJSONObject("data").getJSONObject("user").getString("nick_name")
 
 //                    code : 200 -> 로그인 성공 토스트
 //                    그 외 -> 로그인 실패 토스트
-                    runOnUiThread {
-                    // 백그라운드에서 UI를 쓰려고 하면 오류라 감지하고 앱이 중단된다. 그 해결방안으로 runOnUiThread를 사용한다.
+                        // 백그라운드에서 UI를 쓰려고 하면 오류라 감지하고 앱이 중단된다. 그 해결방안으로 runOnUiThread를 사용한다.
                         if (code == 200) {
-                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
-                        }
-                        else {
+                            val dataObj = jsonObj.getJSONObject("data")
+                            val userObj = dataObj.getJSONObject("user")
+                            val nickname = userObj.getString("nick_name")
+                            runOnUiThread {
+                                Toast.makeText(mContext, "${nickname}님, 환영합니다.", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
                             Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
                         }
 //                            Toast.makeText(mContext, code, Toast.LENGTH_SHORT).show()
 
+
                     }
 
-                }
-
-            })
+                })
         }
 
         binding.btnSignUpLink.setOnClickListener {
