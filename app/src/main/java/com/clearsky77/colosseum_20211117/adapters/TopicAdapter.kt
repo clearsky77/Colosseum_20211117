372e398715221519825b7c59ba41602ad3ac7dec
clearsky77 <clearsky3264@gmail.com>
2021-11-18 16:11:45 +0900

    실제 리스트 데이터 반영. [에러] 화면 안보임.  - Glide 라이브러리 활용

--- app/src/main/java/com/clearsky77/colosseum_20211117/adapters/TopicAdapter.kt
+++ app/src/main/java/com/clearsky77/colosseum_20211117/adapters/TopicAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.clearsky77.colosseum_20211117.R
 import com.clearsky77.colosseum_20211117.datas.TopicData
 
@@ -24,6 +27,14 @@ class TopicAdapter(
         }
 
         var row = tempRow!!
+
+        val data = mList[position]
+
+        val imgTopic = row.findViewById<ImageView>(R.id.imgTopic)
+        val txtTopicTitle = row.findViewById<TextView>(R.id.txtTopicTitle)
+
+        txtTopicTitle.text = data.title
+        Glide.with(mContext).load(data.imageURL).into(imgTopic)
 
         return row
     }
