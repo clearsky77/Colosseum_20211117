12727bd15caa2f9f437c4fc362772cc6033cfb13
clearsky77 <clearsky3264@gmail.com>
2021-11-18 12:21:32 +0900

    ContextUtil: 토큰 값 조회 기능 추가

--- app/src/main/java/com/clearsky77/colosseum_20211117/utils/ContextUtil.kt
+++ app/src/main/java/com/clearsky77/colosseum_20211117/utils/ContextUtil.kt
@@ -11,9 +11,16 @@ class ContextUtil {
         fun setToken(context: Context, token: String){
             // 1. 메모장 열기 prefName에 해당하는 SharedPre열기
             var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE) //이 파일 열어줘
-            // 2. 토큰값을 TOKEN 할ㅇ목에 저장
+            // 2. 토큰값을 TOKEN 항목에 저장
             pref.edit().putString(TOKEN, token).apply()
 
+        }
+
+        fun getToken(context: Context) : String{ // 스트링으로 반환
+            // 1. 메모장 열기 prefName에 해당하는 SharedPre열기
+            var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE) //이 파일 열어줘
+            // 2. 저장된 토큰값을 리턴
+            return pref.getString(TOKEN, "")!! // 어쨌든 얘는 널이 아니야.
         }
     }
 }
